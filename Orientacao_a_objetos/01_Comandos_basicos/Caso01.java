42ab9016a46d5ade4d523589ef85651af4150f2a
David-Henrique <73722144+David-Henrique@users.noreply.github.com>
2020-10-30 19:38:55 -0300

    Create Caso01.java

---
+++ Orientacao_a_objetos/01_Comandos_basicos/Caso01.java
@@ -0,0 +1,16 @@
+package Comandos_basicos;
+
+public class Caso01 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 5;
+		
+		System.out.println("A soma dos valores de a + b = " + (a+b));
+		System.out.println("A soma dos valores de a - b = " + (a-b));
+		System.out.println("A soma dos valores de a * b = " + (a*b));
+		System.out.println("A soma dos valores de a / b = " + (a/b));
+		System.out.println("A soma dso valores de a ^ b = " + (a^b));
+	}
+}
