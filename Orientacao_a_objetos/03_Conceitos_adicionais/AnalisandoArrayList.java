eaed65db72b6ea143d51e7caaaa37aea0a63550c
David-Henrique <73722144+David-Henrique@users.noreply.github.com>
2020-10-30 20:04:35 -0300

    Create AnalisandoArrayList.java

---
+++ Orientacao_a_objetos/03_Conceitos_adicionais/AnalisandoArrayList.java
@@ -0,0 +1,43 @@
+package P3.JDBC.ConexaoComBancoDeDados;
+
+import java.util.ArrayList;
+
+public class AnalisandoArrayList {
+
+	public static void main(String[] args) {
+		
+		//ArrayList
+		
+		ArrayList<String> nomes = new ArrayList<String>();
+		nomes.add("Maria");
+		nomes.add("Joao");
+		nomes.add("Francisca");
+		
+		for(int i = 0; i < nomes.size(); i++) {
+		
+			System.out.println("Nomes; " + nomes.get(i));
+		}
+		
+		System.out.println();
+		
+		// Adicionando novas posicoes na lista
+		
+		nomes.add("Katia");
+		nomes.add("Lauro");
+		
+		for( int i = 0; i < nomes.size(); i++) {
+			System.out.println("Nomes: " + nomes.get(i));
+		}
+		
+		System.out.println();
+		
+		// Removendo posicoes na lista
+		
+		nomes.remove(1);
+		for (int i = 0; i < nomes.size(); i++) {
+			System.out.println("Nomes: " + nomes.get(i));
+		}
+		
+	}
+
+}
